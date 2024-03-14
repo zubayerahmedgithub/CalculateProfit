98425b2e94e26c0a4fff25d0551feb8b834803f4
zubayerahmedgithub <sajib.cu.apece06@gmail.com>
2024-03-14 13:11:06 -0400

    Initial commit

---
+++ src/CalculateProfit.java
@@ -0,0 +1,17 @@
+public class CalculateProfit {
+    public static void main(String[] args) {
+        //Buying Price of An Iphone
+        int buyingPrice = 400;
+
+        //Selling Price Of An Iphone
+        int sellingPrice = 700;
+
+        // Total Profit From Selling
+        System.out.println("Total Profit = $" + (sellingPrice - buyingPrice));
+
+        // Percentage Of Profit
+        System.out.println("Percentage of Profit = $" + (300 * 100 / buyingPrice));
+
+
+    }
+}
